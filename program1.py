9398adfae0b7a4126c4ab345b4d907884f22e7d6
AmanJaiswal1810 <aj3889976@gmail.com>
2024-10-07 12:11:20 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -18,14 +18,14 @@ class Solution:
             depthfirstsearch(i, j - 1)  # Left
         
         
-        island_count = 0
+        icount = 0
         
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 
                 if grid[i][j] == 'L':
-                    island_count += 1
+                    icount += 1
                     depthfirstsearch(i, j)
         
-        return island_count
+        return icount
