b63566c471ddba1e45e9269aedb0ae3dda4fd88e
AmanJaiswal1810 <aj3889976@gmail.com>
2024-10-07 12:08:08 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,6 +1,31 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+        if not grid or not grid[0]:
+            return 0
+        
+        def dfs(i, j):
+            # Boundary and water check
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
+                return
+            
+            # Mark current cell as visited by setting it to 'W'
+            grid[i][j] = 'W'
+            
+            # Explore all four directions
+            dfs(i + 1, j)  # Down
+            dfs(i - 1, j)  # Up
+            dfs(i, j + 1)  # Right
+            dfs(i, j - 1)  # Left
+        
+        # Initialize island count
+        island_count = 0
+        
+        # Iterate through each cell in the grid
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                # Start a DFS for each new island found
+                if grid[i][j] == 'L':
+                    island_count += 1
+                    dfs(i, j)
+        
+        return island_count
