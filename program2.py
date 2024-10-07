387afbae9969d0042e465ba2bff43d15f5c80dea
AmanJaiswal1810 <aj3889976@gmail.com>
2024-10-07 12:15:39 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,5 +1,5 @@
 def decode_message(s: str, p: str) -> bool:
-    # Initialize DP table
+    
     dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
     dp[0][0] = True  # Empty pattern matches empty string
 
