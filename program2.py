0d45a3913e792b87f2718195a9619e75aac56ea6
AmanJaiswal1810 <aj3889976@gmail.com>
2024-10-07 12:19:46 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -3,16 +3,16 @@ def decode_message(s: str, p: str) -> bool:
     Dp_Table = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
     Dp_Table[0][0] = True  
     
-    for j in range(1, len(p) + 1):
-        if p[j - 1] == '*':
-            Dp_Table[0][j] = Dp_Table[0][j - 1]
+    for l in range(1, len(p) + 1):
+        if p[l - 1] == '*':
+            Dp_Table[0][l] = Dp_Table[0][l - 1]
     
     
     for k in range(1, len(s) + 1):
-        for j in range(1, len(p) + 1):
-            if p[j - 1] == '*':
-                Dp_Table[k][j] = Dp_Table[k][j - 1] or Dp_Table[k - 1][j]
-            elif p[j - 1] == '?' or p[j - 1] == s[k - 1]:
-                Dp_Table[k][j] = Dp_Table[k - 1][j - 1]
+        for l in range(1, len(p) + 1):
+            if p[l - 1] == '*':
+                Dp_Table[k][l] = Dp_Table[k][l - 1] or Dp_Table[k - 1][l]
+            elif p[l - 1] == '?' or p[l - 1] == s[k - 1]:
+                Dp_Table[k][l] = Dp_Table[k - 1][l - 1]
     
     return Dp_Table[len(s)][len(p)]
