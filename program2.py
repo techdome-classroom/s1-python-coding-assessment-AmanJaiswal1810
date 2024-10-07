feaa5f42d3701b02d2ca6e7291ab715ca09bcb5d
AmanJaiswal1810 <aj3889976@gmail.com>
2024-10-07 12:18:56 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,19 +1,19 @@
 def decode_message(s: str, p: str) -> bool:
     
-    dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
-    dp[0][0] = True  # Empty pattern matches empty string
+     = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+    [0][0] = True  # Empty pattern matches empty string
 
     
     for j in range(1, len(p) + 1):
         if p[j - 1] == '*':
-            dp[0][j] = dp[0][j - 1]
+            [0][j] = [0][j - 1]
     
     
     for i in range(1, len(s) + 1):
         for j in range(1, len(p) + 1):
             if p[j - 1] == '*':
-                dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+                [i][j] = [i][j - 1] or [i - 1][j]
             elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
-                dp[i][j] = dp[i - 1][j - 1]
+                [i][j] = [i - 1][j - 1]
     
-    return dp[len(s)][len(p)]
+    return [len(s)][len(p)]
